bd52e78df0df2692c96a4b8cf29887869ce0c3af
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2025-01-18 22:30:44 +0300

    Add project files.

---
+++ PatikaCodeFirst/PatikaFirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaCodeFirst.Models;
+using System.Collections.Generic;
+
+namespace PatikaCodeFirst
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=PatikaCodeFirstDb1;Trusted_Connection=True;");
+        }
+    }
+}
